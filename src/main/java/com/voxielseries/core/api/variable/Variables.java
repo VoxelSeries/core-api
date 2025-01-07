b5a3e921f8bb8536e0aa7f81ceb800d9c76ab737
oop778 <oskardhavel@gmail.com>
2025-01-07 10:57:16 +0200

    Create Variables class

---
+++ src/main/java/com/voxielseries/core/api/variable/Variables.java
@@ -0,0 +1,20 @@
+package com.voxielseries.core.api.variable;
+
+import com.voxielseries.core.api.context.IdentityContext;
+import lombok.NonNull;
+import org.intellij.lang.annotations.Pattern;
+
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+public interface Variables<CONTEXT extends IdentityContext<?>> {
+    VariableCreation<CONTEXT> newVariable(@Pattern("[a-z_0-9.]+") String key);
+
+    interface VariableCreation<CONTEXT extends IdentityContext<?>> {
+        void constant(@NonNull Object value);
+
+        void supplied(Supplier<@NonNull Object> valueSupplier);
+
+        void extracted(Function<CONTEXT, @NonNull Object> extractor);
+    }
+}
